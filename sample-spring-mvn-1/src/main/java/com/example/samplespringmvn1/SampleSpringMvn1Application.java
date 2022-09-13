188574a0c265db40114690805204b8a6d0289aa1
荒古外星人 <18381334509@163.com>
2022-09-13 15:09:39 +0800

    Initial commit

---
+++ sample-spring-mvn-1/src/main/java/com/example/samplespringmvn1/SampleSpringMvn1Application.java
@@ -0,0 +1,13 @@
+package com.example.samplespringmvn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMvn1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringMvn1Application.class, args);
+    }
+
+}
